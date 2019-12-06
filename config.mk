a5314bc6a464d6561748557024576dc1199d9e05
jinwen.wang <15029037300@163.com>
2019-12-06 15:04:01 -0800

    makefile support windows

--- config.mk
+++ config.mk
@@ -1,7 +1,11 @@
-ifeq ($(shell uname),Linux)
-$(info this is Linux)
+
+PLATFORM:=$(shell uname)
+$(info this platform is $(PLATFORM))
+ifneq ($(findstring Linux,$(PLATFORM)),)
 TARGET_SUFFIX=
 SHELL:=/bin/bash
+else ifneq ($(findstring NT,$(PLATFORM)),)
+TARGET_SUFFIX=.exe
 endif
 
 VER_MAJOR=1
